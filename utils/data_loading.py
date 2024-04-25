ac91678666a7a192063d75579f5838897447d52d
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-04-25 13:28:33 -0400

    Create data_loading.py

---
+++ utils/data_loading.py
@@ -0,0 +1,99 @@
+# data transformations
+
+flat_transform_augment = transforms.Compose([
+  v2.RandomResizedCrop(224),
+  v2.RandomHorizontalFlip(),
+  v2.AutoAugment(),
+  v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+  transforms.Lambda(lambda x: torch.flatten(x))
+])
+
+std_img_transform = transforms.Compose([
+  v2.RandomResizedCrop(224),
+  v2.RandomHorizontalFlip(),
+  v2.AutoAugment(),
+  v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+flat_transform_basic = transforms.Compose([
+  v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+  transforms.Lambda(lambda x: torch.flatten())
+])
+
+# Datasets
+
+train_data_flat = datasets.CIFAR100(
+    root="data",
+    train=True,
+    download=True,
+    transform=flat_transform_basic,
+    target_transform=None
+)
+
+train_data_flat_augment = datasets.CIFAR100(
+    root="data",
+    train=True,
+    download=True,
+    transform=flat_transform_augment,
+    target_transform=None
+)
+
+train_data_std_img = datasets.CIFAR100(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+train_data_std_img_augment = datasets.CIFAR100(
+    root="data",
+    train=True,
+    download=True,
+    transform=std_img_transform,
+    target_transform=None
+)
+
+test_data_flat = datasets.CIFAR100(
+    root="data",
+    train=False,
+    download=True,
+    transform=flat_transform_basic,
+    target_transform=None
+)
+
+test_data_std_img = datasets.CIFAR100(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+# DataLoaders
+
+train_dataloader_flat = DataLoader(dataset=train_data_flat,
+                                   batch_size=BATCH_SIZE,
+                                   shuffle=True)
+
+train_dataloader_flat_augment = DataLoader(dataset=train_data_flat_augment,
+                                           batch_size=BATCH_SIZE,
+                                           shuffle=True)
+
+train_dataloader_std_img = DataLoader(dataset=train_data_std_img,
+                                      batch_size=BATCH_SIZE,
+                                      shuffle=True)
+
+train_dataloader_std_img_augment = DataLoader(train_data_std_img_augment,
+                                              batch_size_BATCH_SIZE,
+                                              shuffle=True)
+
+test_dataloader_flat = DataLoader(dataset=test_data_flat,
+                                  batch_size=BATCH_SIZE,
+                                  shuffle=False)
+
+test_dataloader_std_img = DataLoader(dataset=test_data_std_img,
+                                     batch_size=BATCH_SIZE,
+                                     shuffle=False)
