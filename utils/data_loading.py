2ad0e07b7b0d022b260734b948c69ecd32630c99
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-05-14 12:09:25 -0400

    Update data_loading.py

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,28 +1,30 @@
-# Data Transformation
-
+import torch
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 import gin
-from torchvision.transforms import v2
 
+# Data Transformation
 @gin.configurable
 def create_transform(transform_type='standard', size=224, normalize=True, flatten=False):
-
     transformations = []
+    
     if transform_type == 'augmented':
         transformations.extend([
-            v2.RandomResizedCrop(size),
-            v2.RandomHorizontalFlip(),
-            v2.AutoAugment(),
+            transforms.RandomResizedCrop(size),
+            transforms.RandomHorizontalFlip(),
+            transforms.AutoAugment(),
         ])
     else:
         transformations.extend([
-            v2.Resize(size),
-            v2.CenterCrop(size),
+            transforms.Resize(size),
+            transforms.CenterCrop(size),
         ])
     
-    transformations.append(v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]))
+    transformations.append(transforms.ToTensor())
     
     if normalize:
-        transformations.append(v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+        transformations.append(transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
     
     if flatten:
         transformations.append(transforms.Lambda(lambda x: torch.flatten(x)))
@@ -30,18 +32,16 @@ def create_transform(transform_type='standard', size=224, normalize=True, flatte
     return transforms.Compose(transformations)
 
 # Datasets
-
 @gin.configurable
 def get_dataset(name='CIFAR100', train=True, transform_config=None):
     dataset_classes = {
         'CIFAR100': datasets.CIFAR100
     }
-    transform = get_transform(**transform_config) if transform_config else None
-    dataset = dataset_classes[name](root='./data', train=train, download=True, transform=transform)
+    transform = create_transform(**transform_config) if transform_config else None
+    dataset = dataset_classes[name](root='data', train=train, download=True, transform=transform)
     return dataset
 
 # DataLoaders
-
 @gin.configurable
 def get_dataloader(dataset, batch_size=32, shuffle=True, num_workers=4):
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
