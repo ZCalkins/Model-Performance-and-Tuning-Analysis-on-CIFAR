6cc89c7fa077b8f9ccc0fe23271cd7c2b6ad368f
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-05-18 15:08:02 -0400

    Update logging_utils.py

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,10 +1,13 @@
 import os
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
+from utils.config_utils import load_yaml_config
 
 def create_writer(experiment_name: str, model_name: str, extra: str=None) -> SummaryWriter:
-    config = load_config()
+    config_file_path = 'CIFAR100-Multi-Model-Ablation-Analysis/configurations/yaml/logging_config.yaml'
+    config = load_yaml_config(config_file_path)
     base_log_dir = config['logging']['base_log_dir']
+    
     timestamp = datetime.now().strftime("%Y-%m-%d")
     if extra:
         log_dir = os.path.join(base_log_dir, timestamp, experiment_name, model_name, extra)
