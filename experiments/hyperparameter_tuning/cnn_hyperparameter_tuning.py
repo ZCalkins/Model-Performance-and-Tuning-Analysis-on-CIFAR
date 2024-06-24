e903c2b7db0d216aba4ddc0fe617fb0b632fc809
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-06-24 13:15:16 -0400

    Update cnn_hyperparameter_tuning.py

--- experiments/hyperparameter_tuning/cnn_hyperparameter_tuning.py
+++ experiments/hyperparameter_tuning/cnn_hyperparameter_tuning.py
@@ -256,7 +256,7 @@ def objective(trial):
     if tensorboard_logger:
         tensorboard_logger.log_hyperparams(trial.params, {'val_loss': val_loss})
 
-    results_file = os.path.join(config['experiment']['save_dir'], 'results.yaml')
+    results_file = os.path.join(config['experiment']['save_dir'], f'results_trial_{trial.number}.yaml')
     os.makedirs(os.path.dirname(results_file), exist_ok=True)
     with open(results_file, 'w') as f:
         yaml.dump({'trial': trial.number, 'params': trial.params, 'val_loss': val_loss, f}
