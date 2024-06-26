821bb67d958188a9e5a7b8cf21f40b176eb38061
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-06-26 13:10:01 -0400

    Update vit_hyperparameter_tuning.py

--- experiments/hyperparameter_tuning/vit_hyperparameter_tuning.py
+++ experiments/hyperparameter_tuning/vit_hyperparameter_tuning.py
@@ -17,7 +17,7 @@ from utils.data_loading import get_dataset, get_dataloader, create_transform
 from models.vit_model import ViTModel, ViTModelConfig
 
 # Load the experiment configuration
-config_file_path = 'configurations/yaml/vit_hyperparameter_tuning.yaml'
+config_file_path = 'configurations/yaml/hyperparameter_tuning/vit_hyperparameter_tuning.yaml'
 with open(config_file_path, 'r') as file:
     config = yaml.safe_load(file)
 
