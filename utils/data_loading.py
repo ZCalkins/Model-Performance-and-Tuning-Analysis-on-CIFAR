daf8e2d3c3318e44e8187c377e731a4bffd66c4f
Zach Calkins <146873488+ZCalkins@users.noreply.github.com>
2024-07-27 15:10:03 -0400

    Update data_loading.py

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -32,7 +32,7 @@ def get_dataset(name='CIFAR100',
             transforms.CenterCrop(size),
         ])
 
-    transformations.append(torchvision.transforms.ToTensor())
+    transformations.append(transforms.ToTensor())
 
     if normalize:
         transformations.append(transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
